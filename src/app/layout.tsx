3c3e70dfbf7b18c6bdcd0766e05ff392446dffd0
Ezequiel Arroyo <ezarroyo@frba.utn.edu.ar>
2026-08-07 12:14:56 -0300

    feat: refactor layout structure by moving Header and Footer components to a separate Layout child component

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,4 @@
 import type { Metadata } from "next";
-import { Footer } from "@/components/layout/footer/Footer";
-import { Header } from "@/components/layout/header/Header";
 import "./globals.css";
 import { hankenGrotesk } from "@/components/ui/fonts";
 
@@ -16,13 +14,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en" className="h-full antialiased">
-      <body className={`${hankenGrotesk.className} antialiased`}>
-        <div className="flex min-h-screen flex-col bg-neutral">
-          <Header />
-          <main className="flex-1 pb-20 md:pb-0">{children}</main>
-          <Footer />
-        </div>
-      </body>
+      <body className={`${hankenGrotesk.className} antialiased`}>{children}</body>
     </html>
   );
 }
