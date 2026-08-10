a2ef936b5958301487abefca7f39237595cd1e3a
Ezequiel Arroyo <ezarroyo@frba.utn.edu.ar>
2026-08-10 10:27:25 -0300

    fix: correct defaultValue assignment in SearchBar component for query parameter

--- src/components/ui/SearchBar.tsx
+++ src/components/ui/SearchBar.tsx
@@ -28,7 +28,7 @@ export function SearchBar({ placeholder }: { placeholder: string }) {
         onChange={(e) => {
           handleSearch(e.target.value);
         }}
-        defaultValue={searchParams.get('query')?.toString()}
+        defaultValue={searchParams.get('query') ?? ''}
       />
     </div>
   );
