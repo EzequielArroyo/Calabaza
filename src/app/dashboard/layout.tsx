0fe91d47f94381b9a843e3fb06f2e7fc77f3b311
Ezequiel Arroyo <ezarroyo@frba.utn.edu.ar>
2026-08-14 14:46:01 -0300

    refactor: update layout component to include SideNav and correct import paths for Header and Footer

--- src/app/dashboard/layout.tsx
+++ src/app/dashboard/layout.tsx
@@ -1,10 +1,14 @@
-import { Footer } from "@/components/layout/footer/Footer";
-import { Header } from "@/components/layout/header/Header";
+import { Footer } from "@/modules/shared/layout/footer/Footer";
+import { Header } from "@/modules/shared/layout/header/Header";
+import SideNav from "@/modules/stores/components/side-nav"
 export default function Layout({ children }: { children: React.ReactNode }) {
   return (
     <div className="flex min-h-screen flex-col bg-neutral">
       <Header />
-      <main className="flex-1 pb-20 md:pb-0">{children}</main>
+      <main className="flex flex-1">
+        <SideNav />
+        <div className="flex-1 pb-20 md:pb-0">{children}</div>
+      </main>
       <Footer />
     </div>
   );
