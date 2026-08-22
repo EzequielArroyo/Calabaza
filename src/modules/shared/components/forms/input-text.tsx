e6d4c0264bae3ad441e234d209752e951ac0e4c9
Ezequiel Arroyo <ezarroyo@frba.utn.edu.ar>
2026-08-22 17:51:24 -0300

    feat: add defaultValue prop to InputText and InputTextarea components

--- src/modules/shared/components/forms/input-text.tsx
+++ src/modules/shared/components/forms/input-text.tsx
@@ -1,6 +1,20 @@
 import { FieldError } from "./field-error";
 
-export default function InputText({ name, label, type, required = false, errorMessage }: { name: string; label: string; type: string; required?: boolean; errorMessage?: string }) {
+export default function InputText({
+  name,
+  label,
+  type,
+  defaultValue,
+  required = false,
+  errorMessage,
+}: {
+  name: string;
+  label: string;
+  type: string;
+  defaultValue?: string | number;
+  required?: boolean;
+  errorMessage?: string;
+}) {
   return (
     <div>
       <label className="block mb-1.5" htmlFor={name}>
@@ -10,9 +24,11 @@ export default function InputText({ name, label, type, required = false, errorMe
         className="bg-background w-full border border-secondary/25 rounded-lg px-3 py-3 outline-none transition focus:border-primary focus:ring-2 focus:ring-primary/20"
         id={name}
         name={name}
-        type= {type}
+        type={type}
+        defaultValue={defaultValue}
         required={required}
       />
       <FieldError message={errorMessage} />
     </div>
-  );}
+  );
+}
--- src/modules/shared/components/forms/input-textarea.tsx
+++ src/modules/shared/components/forms/input-textarea.tsx
@@ -5,12 +5,14 @@ export default function InputTextarea({
   label,
   maxLength = 500,
   required = false,
+  defaultValue,
   errorMessage,
 }: {
   name: string;
   label: string;
   maxLength?: number;
   required?: boolean;
+  defaultValue?: string;
   errorMessage?: string;
 }) {
   return (
@@ -22,6 +24,7 @@ export default function InputTextarea({
         className="bg-background w-full border border-secondary/25 rounded-lg px-3 py-3 outline-none transition focus:border-primary focus:ring-2 focus:ring-primary/20"
         id={name}
         name={name}
+        defaultValue={defaultValue}
         maxLength={maxLength}
         required={required}
       />
