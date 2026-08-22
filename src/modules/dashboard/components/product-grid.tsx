5470b8e602f3955b65d7c0b1ee22851cad7f4f8e
Ezequiel Arroyo <ezarroyo@frba.utn.edu.ar>
2026-08-22 16:13:50 -0300

    feat: enhance ProductGrid to accept query prop for filtering products

--- src/modules/dashboard/components/product-grid.tsx
+++ src/modules/dashboard/components/product-grid.tsx
@@ -1,27 +1,31 @@
 import ProductList from './product-list';
 
-export default function ProductGrid(){
-    return (
-      <div className="rounded-xl border border-outline text-text-secondary shadow-sm overflow-hidden mb-4">
-        <div className="overflow-x-auto">
-          <table className="w-full">
-            <thead className="hidden md:table-header-group border-b border-outline bg-surface uppercase tracking-wider">
-              <tr>
-                <th className="w-12 px-4 py-4">Imagen</th>
-                <th className="px-4 py-4 text-left">Nombre</th>
-                <th className="px-4 py-4 text-left">Categoria</th>
-                <th className="px-4 py-4 text-left">Precio</th>
-                <th className="w-32 px-4 py-4 text-left">Stock</th>
-                <th className="w-32 px-4 py-4 text-left">Estado</th>
-                <th className="w-24 px-4 py-4 text-right">Acciones</th>
-              </tr>
-            </thead>
+export default function ProductGrid({
+  query,
+}: {
+  query: string;
+}) {
+  return (
+    <div className="rounded-xl border border-outline text-text-secondary shadow-sm overflow-hidden mb-4">
+      <div className="overflow-x-auto">
+        <table className="w-full">
+          <thead className="hidden md:table-header-group border-b border-outline bg-surface uppercase tracking-wider">
+            <tr>
+              <th className="w-12 px-4 py-4">Imagen</th>
+              <th className="px-4 py-4 text-left">Nombre</th>
+              <th className="px-4 py-4 text-left">Categoria</th>
+              <th className="px-4 py-4 text-left">Precio</th>
+              <th className="w-32 px-4 py-4 text-left">Stock</th>
+              <th className="w-32 px-4 py-4 text-left">Estado</th>
+              <th className="w-24 px-4 py-4 text-right">Acciones</th>
+            </tr>
+          </thead>
 
-            <tbody className="divide-y divide-outline">
-              <ProductList />
-            </tbody>
-          </table>
-        </div>
+          <tbody className="divide-y divide-outline">
+            <ProductList query={query} />
+          </tbody>
+        </table>
       </div>
-    );
+    </div>
+  );
 }
